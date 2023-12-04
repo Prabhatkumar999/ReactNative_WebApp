7033c5565c9116e7476679b5c837de9ad7f5c1b1
prabhat kumar <115355888+Prabhatkumar999@users.noreply.github.com>
2023-12-04 18:57:31 +0530

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './ReactNative_WebApp/App.js', // Adjust the entry point based on your project structure
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
